00599f38a8a8c59216ce7c56e1e8700e4324c535
Sergei Vladimirov <vladimiroffsergey@gmail.com>
2025-09-10 15:07:10 +0300

    Создание боксплота длин клювов, удаление гистограммы

--- penguins.R
+++ penguins.R
@@ -1,14 +1,6 @@
 data <- read.csv("penguins.csv")
-
 x <- data$bill_length_mm
 
-hist(x, breaks = seq(40, 60, 4))
-hist(x, breaks = seq(40, 60, 2))
-hist(x, breaks = seq(40, 60, 1))
-
-data <- read.csv("penguins.csv")
-x <- data$bill_length_mm
-
-png("penguins-hist.png")
-hist(x, breaks = seq(40, 60, 2))
+png("penguins-boxplot.png")
+boxplot(x)
 dev.off()
