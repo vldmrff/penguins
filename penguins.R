6a61a8290b1f725661bb00ee94d1db681e523510
Sergei Vladimirov <vladimiroffsergey@gmail.com>
2025-09-10 14:47:05 +0300

    Сохранили гипторамму длин клювов

--- penguins.R
+++ penguins.R
@@ -5,3 +5,10 @@ x <- data$bill_length_mm
 hist(x, breaks = seq(40, 60, 4))
 hist(x, breaks = seq(40, 60, 2))
 hist(x, breaks = seq(40, 60, 1))
+
+data <- read.csv("penguins.csv")
+x <- data$bill_length_mm
+
+png("penguins-hist.png")
+hist(x, breaks = seq(40, 60, 2))
+dev.off()
